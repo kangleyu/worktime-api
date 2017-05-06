abc8d6b5cf9737a3d360569e9d6ff2258c5f14a4
Tom Yu <kangleyu@gmail.com>
2017-05-06 13:49:39 +0800

    Add nodemon for project - which can watch for the file changes and auto-restart the server

--- gulp.config.js
+++ gulp.config.js
@@ -1,12 +1,23 @@
 module.exports = function() {
+  /**
+   * Variables
+   */
+  var server = './src/';
+
   var config = {
     /**
      * File Path
      */
     alljs: [
-      './src/**/*.js',
+      server + '**/*.js',
       './*.js'
-    ]
+    ],
+
+    /**
+     * Node Settings
+     */
+    defaultPort: 8010,
+    nodeServer: server + 'server.js'
   };
 
   return config;
--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,43 @@ var args = require('yargs').argv;
 var $ = require('gulp-load-plugins')({ lazy: true });
 var config = require('./gulp.config')();
 
+var port = process.env.PORT || config.defaultPort;
+
 gulp.task('help', $.taskListing);
 gulp.task('default', ['help']);
 
+/**
+ * Verify JavaScript code based on jscs and jshint
+ */
 gulp.task('vet', function() {
   log('Analyzing source with JHint and JSCS...');
 
   return gulp.src(config.alljs)
+    .pipe($.plumber())
     .pipe($.if(args.verbose, $.print()))
     .pipe($.jscs())
     .pipe($.jshint())
     .pipe($.jshint.reporter('jshint-stylish'))
     .pipe($.jshint.reporter('fail'));
+});
+
+/**
+ * Start server
+ */
+gulp.task('serve-dev', function() {
+  var isDev = true;
+
+  var nodeOptions = {
+    script: config.nodeServer,
+    delayTime: 1,
+    env: {
+      'PORT': port,
+      'NODE_ENV': isDev ? 'dev' : 'build'
+    },
+    watch: [config.server]
+  };
+
+  $.nodemon(nodeOptions);
 });
 
 ///////////
